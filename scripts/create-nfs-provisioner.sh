d19bfceb1b54cb7fbf08041e70001d619eee692a
timflannagan1 <timflannagan@gmail.com>
2020-07-02 10:51:28 -0400

    manifests,scripts: Generalize the NFS provisioner scripts.

--- scripts/create-nfs-provisioner.sh
+++ scripts/create-nfs-provisioner.sh
@@ -7,11 +7,11 @@ set -eou pipefail
 # - This assumes the NFS mount location in the manifests/nfs/pod.yaml is group writable
 # - Support deleting and re-creating the PV if that resource already exists
 
-NAMESPACE="${1:-$METERING_NAMESPACE}"
+export NAMESPACE="${1:-$METERING_NAMESPACE}"
 
-if ! kubectl get storageclass sc-tflannag-nfs > /dev/null 2>&1; then
-    echo "Creating the 'sc-tflannag-nfs' StorageClass"
-    kubectl create -f manifests/nfs/storageclass.yaml
+if ! kubectl get storageclass sc-${NAMESPACE}-nfs > /dev/null 2>&1; then
+    echo "Creating the 'sc-${NAMESPACE}-nfs' StorageClass"
+    envsubst < manifests/nfs/storageclass.yaml | kubectl --namespace ${NAMESPACE} create -f -
 fi
 
 if ! kubectl get ns ${NAMESPACE} >/dev/null 2>&1; then
@@ -19,24 +19,24 @@ if ! kubectl get ns ${NAMESPACE} >/dev/null 2>&1; then
     kubectl create ns ${NAMESPACE}
 fi
 
-if ! kubectl --namespace ${NAMESPACE} get service tflannag-nfs-service > /dev/null 2>&1; then
-    kubectl --namespace ${NAMESPACE} create -f manifests/nfs/service.yaml
+if ! kubectl --namespace ${NAMESPACE} get service ${NAMESPACE}-nfs-service > /dev/null 2>&1; then
+    envsubst < manifests/nfs/service.yaml | kubectl --namespace ${NAMESPACE} create -f -
 fi
 
-export CLUSTER_IP=$(kubectl --namespace ${NAMESPACE} get services/tflannag-nfs-service -o jsonpath='{.spec.clusterIP}')
+export CLUSTER_IP=$(kubectl --namespace ${NAMESPACE} get services/${NAMESPACE}-nfs-service -o jsonpath='{.spec.clusterIP}')
 while [[ $? != 0 ]]; do
-    echo "Waiting for the 'tflannag-nfs-service' Service to have a populated spec.ClusterIP"
-    export CLUSTER_IP=$(kubectl --namespace ${NAMESPACE} get services/tflannag-nfs-service --jsonpath='{.spec.clusterIP}')
+    echo "Waiting for the '${NAMESPACE}-nfs-service' Service to have a populated spec.ClusterIP"
+    export CLUSTER_IP=$(kubectl --namespace ${NAMESPACE} get services/${NAMESPACE}-nfs-service --jsonpath='{.spec.clusterIP}')
 done
 
-if ! kubectl --namespace ${NAMESPACE} get pod tflannag-nfs-provisioner > /dev/null 2>&1; then
+if ! kubectl --namespace ${NAMESPACE} get pod ${NAMESPACE}-nfs-provisioner > /dev/null 2>&1; then
     echo "Creating the NFS provisioner Pod"
-    kubectl --namespace ${NAMESPACE} create -f manifests/nfs/pod.yaml
+    envsubst < manifests/nfs/pod.yaml | kubectl --namespace ${NAMESPACE} create -f -
 fi
 
-kubectl wait --for=condition=Ready pod/tflannag-nfs-provisioner --timeout=120s
+kubectl wait --for=condition=Ready pod/${NAMESPACE}-nfs-provisioner --timeout=120s
 
-if kubectl --namespace ${NAMESPACE} get pv pv-tflannag-nfs > /dev/null 2>&1; then
+if kubectl --namespace ${NAMESPACE} get pv pv-${NAMESPACE}-nfs > /dev/null 2>&1; then
     # TODO: support deleting this resource
     echo "Need to manually clean up any existing NFS PV/PVCs"
     exit 1
